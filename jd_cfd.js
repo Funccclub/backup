98ee918529596566989ab4968057478777c9219f
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-06-13 17:00:13 +0800

    Update jd_cfd.js

--- jd_cfd.js
+++ jd_cfd.js
@@ -58,9 +58,9 @@ $.appId = 10009;
   }
   $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
   await requestAlgo();
-  let res = {}, res2 = await getAuthorShareCode("https://raw.githubusercontent.com/gitupdate/updateTeam/master/shareCodes/cfd.json")
-  if (new Date().getHours() <= 3) res = await getAuthorShareCode('http://cdn.annnibb.me/cfd.json');
-  if (!res2) res2 = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/cfd.json')
+  let res = {}, res2 = await getAuthorShareCode("")
+  if (new Date().getHours() <= 3) res = await getAuthorShareCode('');
+  if (!res2) res2 = await getAuthorShareCode('')
   $.strMyShareIds = [...(res && res.shareId || []),...(res2 && res2.shareId || [])]
   $.strGroupIds = [...(res && res.strGroupIds || []),...(res2 && res2.strGroupIds || [])]
   for (let i = 0; i < cookiesArr.length; i++) {
