774adb4136cc096670027286e7d0b8931c8b7b7a
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-06-20 20:08:41 +0800

    Update jd_mohe.js

--- jd_mohe.js
+++ jd_mohe.js
@@ -47,7 +47,7 @@ $.shareId = [];
       '活动地址: https://isp5g.m.jd.com\n' +
       '活动时间：2021-06-2到2021-07-31\n' +
       '更新时间：2021-06-3 12:00');
-  await updateShareCodesCDN()
+
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -503,30 +503,7 @@ function taskurl(url) {
     }
   }
 }
-function updateShareCodesCDN(url = 'https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jd_shareCodes.json') {
-  return new Promise(resolve => {
-    $.get({
-      url ,
-      timeout: 10000,
-      headers:{"User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")}}, async (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(`${$.name} API请求失败，请检查网路重试`)
-        } else {
-          $.updatePkActivityIdRes = JSON.parse(data);
-          if ($.updatePkActivityIdRes && $.updatePkActivityIdRes.length) {
-            // $.shareId = $.updatePkActivityIdRes || [];
-          }
-        }
-      } catch (e) {
-        $.logErr(e, resp)
-      } finally {
-        resolve();
-      }
-    })
-  })
-}
+
 function TotalBean() {
   return new Promise(async resolve => {
     const options = {
