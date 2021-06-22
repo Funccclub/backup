c42d0239666919083928c53a932178970c055383
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-06-22 09:47:51 +0800

    Update jd_cfd.js

--- jd_cfd.js
+++ jd_cfd.js
@@ -1153,7 +1153,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://share.turinglabs.net/api/v3/jxcfd/query/${randomCount}/`,
+      url: ``,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
