1c7d603aa3e0b0215e0f49f28eee082949ab73cd
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-06-22 10:55:27 +0800

    Update jd_small_home.js

--- jd_small_home.js
+++ jd_small_home.js
@@ -77,7 +77,7 @@ const JD_API_HOST = 'https://lkyl.dianpusoft.cn/api';
       await smallHome();
     }
   }
-  await updateInviteCodeCDN('https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jd_updateSmallHomeInviteCode.json');
+  await updateInviteCodeCDN('');
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -776,7 +776,7 @@ function login(userName) {
     })
   })
 }
-function updateInviteCode(url = 'https://raw.githubusercontent.com/LXK9301/updateTeam/master/jd_updateSmallHomeInviteCode.json') {
+function updateInviteCode(url = '') {
   return new Promise(resolve => {
     $.get({url}, async (err, resp, data) => {
       try {
