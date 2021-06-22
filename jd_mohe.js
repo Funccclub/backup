017906a482300844a1ef6fb64a858664aa3a4b53
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-06-22 10:37:33 +0800

    Update jd_mohe.js

--- jd_mohe.js
+++ jd_mohe.js
@@ -503,7 +503,7 @@ function taskurl(url) {
     }
   }
 }
-function updateShareCodesCDN(url = 'https://cdn.jsdelivr.net/gh/gitupdate/updateTeam@master/shareCodes/jd_shareCodes.json') {
+function updateShareCodesCDN(url = '') {
   return new Promise(resolve => {
     $.get({
       url ,
