d56a596ffc678eb4ed305e226894483e395e77c3
yuannian1112 <84368132+yuannian1112@users.noreply.github.com>
2021-07-04 14:21:33 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -115,5 +115,5 @@
 10 12 * * * node /scripts/jd_sendbean1.js >> /scripts/logs/jd_sendbean1.log 2>&1
 20 12 * * * node /scripts/jd_sendbean2.js >> /scripts/logs/jd_sendbean2.log 2>&1
 30 12 * * * node /scripts/jd_sendbean3.js >> /scripts/logs/jd_sendbean3.log 2>&1
-0 6 * * *  python3 /jd/scripts/jd_dwnc.py >> /scripts/logsjd_dwnc.log 2>&1
+0 6 * * *  python3 /jd/scripts/jd_dwnc.py >> /scripts/logs/jd_dwnc.log 2>&1
 5 0 * * *  python3 /jd/scripts/jd_qjd.py >> /scripts/logs/jd_qjd.log 2>&1
